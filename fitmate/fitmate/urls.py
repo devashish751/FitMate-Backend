e6f6e66c7318ee52351f8e44942b697f68a71a68
Final Year Design Project <fydp@sillicon.ca>
2012-05-17 14:04:04 -0400

    Added django project

---
+++ fitmate/fitmate/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'fitmate.views.home', name='home'),
+    # url(r'^fitmate/', include('fitmate.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
